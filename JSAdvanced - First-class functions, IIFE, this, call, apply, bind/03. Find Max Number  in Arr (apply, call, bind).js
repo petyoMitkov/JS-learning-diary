71eadee22aeba3161b8cc4113703e9dc6e73ba1d
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-02-15 23:30:22 +0200

    Added file about apply()

---
+++ JSAdvanced - First-class functions, IIFE, this, call, apply, bind/03. Find Max Number  in Arr (apply, call, bind).js
@@ -0,0 +1,5 @@
+function commandProcessor_FindMaxNum(arr) {
+	console.log(Math.max.apply(this, arr));
+}
+commandProcessor_FindMaxNum([5, 6, 3, 65, -150, 225]);
+commandProcessor_FindMaxNum([5, 600, 3, 95, -50, 59, 100]);

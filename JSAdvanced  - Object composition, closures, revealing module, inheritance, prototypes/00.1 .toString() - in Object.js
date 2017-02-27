dff3ee15b87352caf0f6078688141efbef558db1
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-02-27 23:49:57 +0200

    Convert indentation to Spaces

---
+++ JSAdvanced  - Object composition, closures, revealing module, inheritance, prototypes/00.1 .toString() - in Object.js
@@ -0,0 +1,34 @@
+// .toString() - function in Obj
+let rect = {
+    width: 10, 
+    height: 5,
+    grow: function(w, h) {
+        this.width += w;
+        this.height += h; 
+    },
+    toString: function() {
+        return `rect[${this.width} x ${this.height}]`;
+    }
+};
+
+console.log(rect);
+console.log("Text + Obj return .toString() : " + rect);
+
+let rect2 = {
+    width: 23420, 
+    height: 3425,
+    grow: function(w, h) {
+        this.width += w;
+        this.height += h; 
+    },
+    toString: function() {
+        return `rect2[${this.width} x ${this.height}]`;
+    }
+};
+
+console.log(rect2);
+console.log("Text + Obj return .toString() : " + rect2);
+
+let joinToArr = [rect,rect2].join(" , ");
+console.log("[rect,rect2].join(' , ') = "  +  joinToArr);
+//console.log(joinToArr);
---
+++ JSAdvanced  - Object composition, closures, revealing module, inheritance, prototypes/02. Fibonacci numbers (Closure, IIFE - Examples).js
@@ -0,0 +1,31 @@
+//EXAMPLES - Closure, IIFE, Fibonacci Numbers
+
+//Closure
+function counterClosure() {
+    let counter = 0;
+    function addFive() {
+        console.log(counter += 5);
+    }
+    return addFive;
+}
+let newCounter = counterClosure();
+
+//Closure + IIFE
+let counter = (function(start) {
+    let num = start;
+    return function() {
+        console.log(++num);
+    }
+})(10);
+
+//Fibonachi Numbers 
+let fibonacci = (function() {
+    let lastNum1 = 0;
+    let lastNum2 = 1;
+    return function() {
+        let next = lastNum1 + lastNum2;
+        console.log(next);
+        lastNum1 = lastNum2;
+        lastNum2 = next; 
+    }
+})();

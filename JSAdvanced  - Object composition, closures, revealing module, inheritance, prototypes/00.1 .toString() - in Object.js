00dd552b20b66ad9ff767d4e2717206ed9798ded
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-02-27 23:42:44 +0200

    Added Sum by Module pattern and convert indentation to spaces

--- JSAdvanced  - Object composition, closures, revealing module, inheritance, prototypes/00.1 .toString() - in Object.js
+++ JSAdvanced  - Object composition, closures, revealing module, inheritance, prototypes/00.1 .toString() - in Object.js
@@ -1,29 +1,29 @@
 // .toString() - function in Obj
 let rect = {
-	width: 10, 
-	height: 5,
-	grow: function(w, h) {
-		this.width += w;
-		this.height += h; 
-	},
-	toString: function() {
-		return `rect[${this.width} x ${this.height}]`;
-	}
+    width: 10, 
+    height: 5,
+    grow: function(w, h) {
+        this.width += w;
+        this.height += h; 
+    },
+    toString: function() {
+        return `rect[${this.width} x ${this.height}]`;
+    }
 };
 
 console.log(rect);
 console.log("Text + Obj return .toString() : " + rect);
 
 let rect2 = {
-	width: 23420, 
-	height: 3425,
-	grow: function(w, h) {
-		this.width += w;
-		this.height += h; 
-	},
-	toString: function() {
-		return `rect2[${this.width} x ${this.height}]`;
-	}
+    width: 23420, 
+    height: 3425,
+    grow: function(w, h) {
+        this.width += w;
+        this.height += h; 
+    },
+    toString: function() {
+        return `rect2[${this.width} x ${this.height}]`;
+    }
 };
 
 console.log(rect2);

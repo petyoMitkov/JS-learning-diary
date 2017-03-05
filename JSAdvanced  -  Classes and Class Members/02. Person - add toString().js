d4f28d09941d05260c39540711fa84b62df40556
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-03-05 18:41:14 +0200

    Added toString(); Return Arr of Person instance

---
+++ JSAdvanced  -  Classes and Class Members/02. Person - add toString().js
@@ -0,0 +1,15 @@
+class Person {
+    constructor(firstName, lastName, age, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+    }
+}
+
+let petyo = new Person("Petyo", "Mitkov", 32, "petyo@gmail.com");
+console.log(petyo.toString());

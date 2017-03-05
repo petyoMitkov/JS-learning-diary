d57c7d33bfaf393c2fa28747b929e63ff7e9880c
Petyo Mitkov <petyo.mitkov@gmail.com>
2017-03-05 17:40:51 +0200

    Create Class with Method

---
+++ JSAdvanced  -  Classes-and Class Members/01. Rectangle - create Class with Method.js
@@ -0,0 +1,13 @@
+class Rectangle {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    calcArea() {
+        return this.width * this.height;
+    }
+}
+
+let rect = new Rectangle(4, 5, "red");
